1dea1cd4fcf3381a5aae105c89e8c26beeb0accb
bencavat <61764851+bencavat@users.noreply.github.com>
2020-03-24 14:06:04 -0400

    Add files via upload

--- Athena.py
+++ Athena.py
@@ -9,16 +9,42 @@ query = "SELECT * FROM wine"
 database = "wine_feb_24"
 s3_output = "s3://winebenathenaoutput/"
 
-def run_query(query, database, s3_output):
+def submit_query(query, database, s3_output):
     client = boto3.client('athena')
     response = client.start_query_execution(
         QueryString=query,
         QueryExecutionContext={
             'Database': database
-            },
+        },
         ResultConfiguration={
             'OutputLocation': s3_output,
-            }
-        )
+        }
+    )
     print('Execution ID: ' + response['QueryExecutionId'])
     return response
+
+def wait_for_results(execution_ID):
+    # while loop
+        # time module: wait a bit
+    return status
+
+def get_results(status_message):
+
+
+
+def run_query():
+    response = submit_query(query, database, s3_output)
+    status = wait_for_results(response)
+    if status == "FAILED" | status == "CANCELLED":
+        print("query over")
+        return 0
+    elif status == "SUCCEEDED":
+        get_results(status)
+    else:
+        print(status)
+        print(response)
+
+
+if __name__ == "__main__":
+    run_query()
+    print("tada!")
