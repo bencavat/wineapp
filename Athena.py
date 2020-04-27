5d2768f3e4bd2fbc5d1079c8951dc277c4a61527
Ben <benjamincavat@gmail.com>
2020-04-26 23:08:28 -0400

    V1 wait for reponse
    * added time module
    * expanded wait for reponse

--- Athena.py
+++ Athena.py
@@ -1,40 +1,45 @@
 import boto3
+import time
 
 #s3 = boto3.resource('s3')
 #for bucket in s3.buckets.all():
 #    print(bucket.name)
 
-
+client = boto3.client('athena')
 query = "SELECT * FROM wine"
 database = "wine_feb_24"
 s3_output = "s3://winebenathenaoutput/"
 
 def submit_query(query, database, s3_output):
-    client = boto3.client('athena')
     response = client.start_query_execution(
-        QueryString=query,
-        QueryExecutionContext={
-            'Database': database
-        },
-        ResultConfiguration={
-            'OutputLocation': s3_output,
-        }
+        QueryString=query, QueryExecutionContext={'Database': database},
+        ResultConfiguration={'OutputLocation': s3_output}
     )
     print('Execution ID: ' + response['QueryExecutionId'])
     return response
 
+
 def wait_for_results(execution_ID):
-    # while loop
-        # time module: wait a bit
-    return status
+    print(execution_ID)
+    for i in range(0,3):
+        print("Query sent, waiting 10 seconds")
+        time.sleep(10)
+        try:
+            response = client.get_named_query(NamedQueryId=execution_ID)
+            if reponse:
+                return response
+                break
+        except:
+            pass
 
 def get_results(status_message):
     print("results")
 
 def run_query():
     response = submit_query(query, database, s3_output)
-    status = wait_for_results(response)
-    if status == "FAILED" | status == "CANCELLED":
+    print(f'{response}')
+    status = wait_for_results(response['QueryExecutionId'])
+    if status == "FAILED" or status == "CANCELLED":
         print("query over")
         return 0
     elif status == "SUCCEEDED":
