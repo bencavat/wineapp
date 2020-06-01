de59592971bcf3676f90dd88e421b813a00cce6f
Ben <benjamincavat@gmail.com>
2020-06-01 14:42:52 -0400

    post fddc 1on1 need parse json

--- Athena.py
+++ Athena.py
@@ -2,6 +2,7 @@
 
 import boto3
 import time
+import sys
 
 client = boto3.client('athena')
 query = "SELECT * FROM wine WHERE acidity >= 2"
@@ -17,11 +18,8 @@ def submit_query(query, database, s3_output):
     print('Execution ID: ' + response['QueryExecutionId'])
     return response
 
-
+"""
 def wait_for_results(execution_ID):
-    # while loop
-        # time module: wait a bit
-    #return status\
     for i in range(0,3):
         print("Query sent, waiting 10 seconds")
         time.sleep(10)
@@ -34,8 +32,46 @@ def wait_for_results(execution_ID):
                 print("Trying again")
         except:
             return 0
+"""
 
+
+def wait_for_results(execution_ID):
+    for i in range(0,5):
+        try:
+            response = client.get_query_execution(QueryExecutionId=execution_ID)
+            status = response['QueryExecution']['Status']['State']
+            print(status)
+            if status == "SUCCEEDED" or status == "FAILED" or status == "CANCELLED":
+                return status
+            else:
+                time.sleep(10)
+        except:
+            return 0
+
+
+def get_query_results(response):
+    status = wait_for_results(response['QueryExecutionId'])
+    if status == "SUCCEEDED":
+        results = client.get_query_results(QueryExecutionId=response['QueryExecutionId'])
+        print("Success!")
+        # now you gotta parse the json
+        # give random wines
+        print(results)
+    elif status == "FAILED" or status == "CANCELLED":
+        print(f"query over: status is {status}")
+        sys.exit()
+    else:
+        results = client.get_query_results(QueryExecutionId=response['QueryExecutionId'])
+        print("couldn't assess status")
+        print(f'Status is:\n {status}')
+        print(f'Response is:\n {response}')
+        print(f'results is:\n {results}')
+
+
+"""
 def run_query(response):
+    #rename get_query_results(response)
+    #integrate wait for results
     status, results = wait_for_results(response['QueryExecutionId'])
     if status == "FAILED" or status == "CANCELLED":
         print("query over")
@@ -48,10 +84,10 @@ def run_query(response):
         print(f'Status is:\n {status}')
         print(f'Response is:\n {response}')
         print(f'results is:\n {results}')
-
+"""
 def main():
     response = submit_query(query, database, s3_output)
-    run_query(response)
+    get_query_results(response)
 
 
 if __name__ == "__main__":
