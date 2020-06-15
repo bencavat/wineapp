90eefa3d65f79ea0c398ec74972f95484604e320
Ben <benjamincavat@gmail.com>
2020-06-14 23:44:51 -0400

    created raw data dump from Athena

--- Athena.py
+++ Athena.py
@@ -3,13 +3,14 @@
 import boto3
 import time
 import sys
-from wineapp.jsonparsing import dump_data
+import json
+#from wineapp.jsonparsing import dump_data
 
 client = boto3.client('athena')
 query = "SELECT * FROM wine WHERE acidity >= 2"
 database = "wine_feb_24"
 s3_output = "s3://winebenathenaoutput/"
-
+savefile = "raw data"
 
 def submit_query(query, database, s3_output):
     response = client.start_query_execution(
@@ -19,21 +20,10 @@ def submit_query(query, database, s3_output):
     print('Execution ID: ' + response['QueryExecutionId'])
     return response
 
-"""
-def wait_for_results(execution_ID):
-    for i in range(0,3):
-        print("Query sent, waiting 10 seconds")
-        time.sleep(10)
-        try:
-            status = client.get_query_execution(QueryExecutionId=execution_ID)
-            if status:
-                results = client.get_query_results(QueryExecutionId=execution_ID)
-                return status, results
-            else:
-                print("Trying again")
-        except:
-            return 0
-"""
+
+def save_results(results, savefile):
+    with open(savefile, 'w') as f:
+        json.dump(results, f, indent=2)
 
 
 def wait_for_results(execution_ID):
@@ -58,6 +48,7 @@ def get_query_results(response):
         # now you gotta parse the json
         # give random wines
         print(results)
+        return results
     elif status == "FAILED" or status == "CANCELLED":
         print(f"query over: status is {status}")
         sys.exit()
@@ -69,26 +60,11 @@ def get_query_results(response):
         print(f'results is:\n {results}')
 
 
-"""
-def run_query(response):
-    #rename get_query_results(response)
-    #integrate wait for results
-    status, results = wait_for_results(response['QueryExecutionId'])
-    if status == "FAILED" or status == "CANCELLED":
-        print("query over")
-        return 0
-    elif status == "SUCCEEDED":
-        print("Success?")
-        print(results)
-    else:
-        print("couldn't assess status")
-        print(f'Status is:\n {status}')
-        print(f'Response is:\n {response}')
-        print(f'results is:\n {results}')
-"""
 def main():
     response = submit_query(query, database, s3_output)
-    get_query_results(response)
+    results = get_query_results(response)
+    print(f'Type of results is {type(results)}')
+    [save_results(results, savefile) if results else print("No results, no savefile")]
 
 
 if __name__ == "__main__":
